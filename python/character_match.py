f840167c71edc3a74639083d135670a65342b8c6
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 17:06:51 -0500

    working python

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,17 @@
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	string1 = string1.upper()
+	string2 = string2.upper()
+	arr1 = list(''.join(string1.split(' ')))
+	arr1.sort()
+	arr2 = list(''.join(string2.split(' ')))
+	arr2.sort()
+	if len(arr1) != len(arr2):
+		return('false')
+	for i in range(0, len(arr1)):
+		if arr1[i] != arr2[i]:
+			return('false')
+	return('true')
+
+print(is_character_match('zach', 'attack'))	
